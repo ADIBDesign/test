daef6ef4dc72f3e2be7ab42f6c18129a8b2e4822
ADIBDesign <118874089+ADIBDesign@users.noreply.github.com>
2022-11-24 12:37:36 +0400

    change

--- build/ios-swift/StyleDictionaryColor.swift
+++ build/ios-swift/StyleDictionaryColor.swift
@@ -4,7 +4,7 @@
 //
 
 // Do not edit directly
-// Generated on Wed, 23 Nov 2022 14:39:02 GMT
+// Generated on Thu, 24 Nov 2022 08:31:08 GMT
 
 
 import UIKit
@@ -16,7 +16,7 @@ public enum StyleDictionaryColor {
     public static let brandSecondaryOne = UIColor(red: 0.776, green: 0.776, blue: 0.773, alpha: 1)
     public static let brandSecondaryThree = UIColor(red: 0.000, green: 0.757, blue: 0.871, alpha: 1)
     public static let brandSecondaryTwo = UIColor(red: 0.549, green: 0.439, blue: 0.294, alpha: 1)
-    public static let buttonBackground = UIColor(red: 0.000, green: 0.620, blue: 0.890, alpha: 1)
+    public static let buttonBackground = UIColor(red: 0.549, green: 0.439, blue: 0.294, alpha: 1)
     public static let buttonText = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
     public static let interaction = UIColor(red: 0.000, green: 0.522, blue: 1.000, alpha: 1)
     public static let primitiveBlack = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
